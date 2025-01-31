b62a1803e45935938808d0a74c54f3e6e20a86c3
Giovanni Benedet Rodrigues <gi-benedet@hotmail.com>
2025-01-30 21:51:34 -0300

    desenvolvendo o programa
    agora podemos pegar as coordenadas e converter para endereço

--- Ziel_App/main.py
+++ Ziel_App/main.py
@@ -3,6 +3,8 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.animation import Animation
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.lang import Builder
+from plyer import gps
+from geopy.geocoders import Nominatim
 
 
 class Ruido_Ambiental_Window(Screen):
@@ -25,6 +27,25 @@ class HomeApp(App):
     def animar_cor(self, widget, nova_cor):
         anim = Animation(background_color=nova_cor, duration=0.01)
         anim.start(widget)
+    
+    def get_location(self, instance):
+        try:
+            gps.configure(on_location=self.on_location, on_status=self.on_status)
+            gps.start()
+        except NotImplementedError:
+            self.root.ids.location_label.text = "GPS não suportado neste dispositivo"
+    
+    def on_location(self, **kwargs):
+        self.root.ids.location_label.text = f"Latitude: {kwargs['lat']}, Longitude: {kwargs['lon']}"
+        gps.stop()
+    
+    def on_status(self, stype, status):
+        self.root.ids.location_label.text = f"Status do GPS: {status}"
+
+    def corvert(self):
+        locator = Nominatim(user_agent="myGeocoder")
+        location = locator.geocode(self.root.ids.location_label.text)
+        self.root.ids.endereco_label.text = f"{self.root.ids.location_label.text}"
 
 if __name__ == '__main__':
     HomeApp().run()
