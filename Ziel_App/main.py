e52b29a54188e7debbac347b7db79846fe823235
Giovanni Benedet Rodrigues <gi-benedet@hotmail.com>
2025-02-05 11:33:25 -0300

    correção do get_location
    estava mal localizado as funções junta mente com os label resultando em resultado nenhum

--- Ziel_App/main.py
+++ Ziel_App/main.py
@@ -8,7 +8,16 @@ from geopy.geocoders import Nominatim
 
 
 class Ruido_Ambiental_Window(Screen):
-    pass
+
+    def on_tipo_ruido(self, instance):
+
+        for btn in self.ids.toggle_group.children:
+            btn.background_color = (1, 1, 1, 1)  # Branco (RGBA)
+        
+        # Muda a cor da opção selecionada
+        instance.background_color = (0, 1, 0, 1)  # Verde (RGBA)
+        print(f"Selecionado: {instance.text}")
+
 
 class Home_Window(Screen):
     pass
@@ -28,24 +37,31 @@ class HomeApp(App):
         anim = Animation(background_color=nova_cor, duration=0.01)
         anim.start(widget)
     
-    def get_location(self, instance):
+    def get_location(self):
         try:
             gps.configure(on_location=self.on_location, on_status=self.on_status)
             gps.start()
         except NotImplementedError:
-            self.root.ids.location_label.text = "GPS não suportado neste dispositivo"
+            screen = self.root.get_screen("Ruido_Ambiental")
+            screen.ids["location_label"].text = "GPS não suportado neste dispositivo"
     
     def on_location(self, **kwargs):
-        self.root.ids.location_label.text = f"Latitude: {kwargs['lat']}, Longitude: {kwargs['lon']}"
+        """Este método será chamado quando a localização for encontrada"""
+        lat = kwargs.get("lat", "N/A")  # Obtém latitude (ou "N/A" se não existir)
+        lon = kwargs.get("lon", "N/A")  # Obtém longitude (ou "N/A" se não existir)
+        screen = self.root.get_screen("Ruido_Ambiental")
+        screen.ids["location_label"].text = f"Latitude: {lat}, Longitude: {lon}"
         gps.stop()
     
     def on_status(self, stype, status):
-        self.root.ids.location_label.text = f"Status do GPS: {status}"
+        screen = self.root.get_screen("Ruido_Ambiental")
+        screen.ids["location_label"].text = f"Status do GPS: {status}"
 
     def corvert(self):
         locator = Nominatim(user_agent="myGeocoder")
-        location = locator.geocode(self.root.ids.location_label.text)
-        self.root.ids.endereco_label.text = f"{self.root.ids.location_label.text}"
+        location = locator.geocode(self.root.ids["location_label"].text)
+        screen = self.root.get_screen("Ruido_Ambiental")
+        screen.ids["endereco_label"].text = f"{screen.ids["location_label"].text}"
 
 if __name__ == '__main__':
     HomeApp().run()
